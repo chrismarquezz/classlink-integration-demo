0c395d74e78dbc3647d7dd53b2601b9c4f125e5a
chrismarquezz <chrismarquez00@icloud.com>
2025-07-16 18:57:01 -0400

    Weekly changes

--- frontend/src/components/ProfileDropdown.jsx
+++ frontend/src/components/ProfileDropdown.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 
+// This component now receives the user profile from our database and an onSignOut function
 function ProfileDropdown({ user, onSignOut }) {
+  // This check is still important
   if (!user) {
     return null;
   }
 
-  const email = user.attributes?.email;
-  const username = user.username;
+  // Read the properties from our database user object
+  const firstName = user.firstName;
+  const lastName = user.lastName;
+  const email = user.email;
+  const role = user.role;
 
   return (
     <div className="profile-dropdown">
       <div className="dropdown-item">
-        <strong>Username:</strong> {username}
+        <strong>Name:</strong> {firstName} {lastName}
       </div>
-      {email && (
+      {email && email !== 'N/A' && (
         <div className="dropdown-item">
           <strong>Email:</strong> {email}
         </div>
       )}
+      <div className="dropdown-item">
+        <strong>Role:</strong> {role}
+      </div>
+      {/* The Sign Out button now correctly calls the onSignOut function */}
       <div className="dropdown-item logout" onClick={onSignOut}>
         Sign Out
       </div>
