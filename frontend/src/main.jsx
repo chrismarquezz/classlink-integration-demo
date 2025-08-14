f8e55d6e0b72d12f111a38b34e3c626d2ed05930
chrismarquezz <chrismarquez00@icloud.com>
2025-08-13 21:13:27 -0400

    Got rid of old code in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,28 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { Amplify } from 'aws-amplify';
-
-const amplifyConfig = {
-  Auth: {
-    Cognito: {
-      userPoolId: 'us-east-1_0VYziM7It',
-      userPoolClientId: '2s9aedlljf2jhfav0up5d7b23d',
-      loginWith: {
-        oauth: {
-          domain: 'us-east-10vyzim7it.auth.us-east-1.amazoncognito.com',
-          scopes: ['openid'],
-          redirectSignIn: ['http://localhost:5173'],
-          redirectSignOut: ['http://localhost:5173'],
-          responseType: 'code'
-        }
-      }
-    }
-  }
-};
-
-Amplify.configure(amplifyConfig);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
